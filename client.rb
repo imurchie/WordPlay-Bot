80608234ad4d464106d3a2f7c3e8f5295803a29b
Isaac Murchie <imurchie@berkeley.edu>
2013-05-15 10:50:35 -0700

    Two bots talk to each other

--- client.rb
+++ client.rb
@@ -6,12 +6,14 @@
 require_relative "bot.rb"
 
 
-bot = Bot.new(:name => "Fred", :data_file => "fred.bot")
+fred = Bot.new(:name => "Fred", :data_file => "fred.bot")
+charlie = Bot.new(:name => "Charlie", :data_file => "fred.bot")
 
-puts bot.greeting
+r = fred.greeting
 
-while input = gets and input.chomp != "end"
-  puts ">> " + bot.response_to(input)
+10.times do
+  puts "#{fred.name}: #{r}"
+  r = charlie.response_to(r)
+  puts "#{charlie.name}: #{r}"
+  r = fred.response_to(r)
 end
-
-puts bot.farewell
